55a66e23da405ee5b6934962a8f925d0332813f2
FrankieLee1997 <1912088450@qq.com>
2020-03-05 15:09:11 +0800

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,17 @@
+package com.lfy.test;
+
+public class Fibonacci {
+    public static int of(int num) {
+        if (num == 1 || num == 2)
+            return 1;
+        else
+            return of(num - 1) + of(num - 2);
+
+    }
+
+    public static void main(String[] args) {
+        for (int i = 1; i < 200; i++) {
+            System.out.println(String.format("Fibonacci.of(%s) == ",i)+of(i));
+        }
+    }
+}
